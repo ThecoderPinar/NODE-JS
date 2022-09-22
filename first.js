f0dc5cc1d94d8be89f5abe1c36b0679d7dedde85
Pınar <107423523+ThecoderPinar@users.noreply.github.com>
2022-09-22 13:03:19 +0300

    Update first.js

--- first.js
+++ first.js
@@ -1,8 +1,9 @@
 const params = process.argv.slice(2);
 
-function circlearea (r){
-    let area = Math.floor(Math.PI)*(r*r)
-    console.log('Yarı çapı ${12} olan dairenin alanı: ${452.16}')
+function circlearea (radius){
+    const area = Math.PI.toFixed(2) * radius ** 2;
+    return console.log('Yarı çapı ${12} olan dairenin alanı: ${452.16}')
 }
 
-circlearea(params[0]*1);
+circlearea(params[0] * 1);
+
